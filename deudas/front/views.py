14e8b3b63d50b79118180ef8710cdd74e65a5b4d
Ximena Contreras <satelliteg@gmail.com>
2014-07-19 12:12:30 -0400

    Filtro por Fecha

--- deudas/front/views.py
+++ deudas/front/views.py
@@ -1,4 +1,6 @@
 import datetime
+import locale
+import calendar
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.views.generic import View, TemplateView, FormView
@@ -30,46 +32,55 @@ class List(TemplateView):
 		context["duenios"] = models.Cliente.objects.values_list("duenio").distinct()
 		context["listCliente"] = models.Cliente.objects.all().order_by("pk")
 		context["listGlosa"] = models.Glosa.objects.all().order_by("pk")
-		context["clienteGlosa"] = tabla(context["listCliente"],context["listGlosa"])
+		context["clienteGlosa"] = tabla(context["listCliente"],context["listGlosa"],datetime.date.today())
+		dates = models.Ingreso.objects.values_list("fecha").distinct()
+		locale.setlocale(locale.LC_TIME, 'es_ES')
+		datesList = map(lambda date: date[0].strftime("%B - %Y"), reversed(dates))
+		context["dates"] = list(set(datesList))
+		print(datetime.datetime.strptime(context["dates"][0],"%B - %Y"))
 		return context
 
 	@method_decorator(login_required)
 	def dispatch(self, request, *args, **kwargs):
 		return super(self.__class__, self).dispatch(request, *args, **kwargs)
 
-def tabla(listCliente,listGlosa):
-	month = datetime.date.today().month
-	year = datetime.date.today().year
+def tabla(listCliente,listGlosa, date):
+	month = date.month
+	year = date.year
+	datet = date.replace(day = calendar.monthrange(year, month)[1])
+	datel = date.replace(day = 1)
+	print(datel)
+	print(datet)
 	return map(
 		(lambda cliente:
 			[cliente.nombre,
 			map(lambda glosa:
-				models.Ingreso.objects.filter(cliente=cliente,glosa=glosa,fecha__month=month,fecha__year=year).aggregate(Sum('valor'))["valor__sum"],
+				models.Ingreso.objects.filter(cliente=cliente,glosa=glosa,fecha__lt=datet, fecha__gt=datel).aggregate(Sum('valor'))["valor__sum"],
 			listGlosa),
-			deuda(cliente,"deuda",month, year),
-			deuda__other(cliente,"deuda",month, year) - deuda__other(cliente,"boleta",month, year),
-			deuda(cliente,"boleta",month, year),
-			deuda_total(cliente,"deuda") - deuda_total(cliente,"boleta"),
+			deuda(cliente,"deuda",datet,datel),
+			deuda__other(cliente,"deuda",datel) - deuda__other(cliente,"boleta",datel),
+			deuda(cliente,"boleta",datet,datel),
+			deuda_total(cliente,"deuda",datet) - deuda_total(cliente,"boleta",datet),
 			cliente.pk]),
 		listCliente)
 
-def deuda(cliente, tipo, month, year): 
-	if models.Ingreso.objects.filter(cliente=cliente,tipo=tipo,fecha__month=month,fecha__year=year).aggregate(Sum('valor'))["valor__sum"] == None: 
+def deuda(cliente, tipo, datet, date): 
+	if models.Ingreso.objects.filter(cliente=cliente,tipo=tipo,fecha__lt=datet, fecha__gt=date).aggregate(Sum('valor'))["valor__sum"] == None: 
 		return 0
 	else:
-		return models.Ingreso.objects.filter(cliente=cliente,tipo=tipo,fecha__month=month,fecha__year=year).aggregate(Sum('valor'))["valor__sum"]
+		return models.Ingreso.objects.filter(cliente=cliente,tipo=tipo,fecha__lt=datet, fecha__gt=date).aggregate(Sum('valor'))["valor__sum"]
 
-def deuda__other(cliente, tipo, month, year): 
-	if models.Ingreso.objects.filter(cliente=cliente,tipo=tipo).exclude(fecha__month=month,fecha__year=year).aggregate(Sum('valor'))["valor__sum"] == None: 
+def deuda__other(cliente, tipo, date): 
+	if models.Ingreso.objects.filter(cliente=cliente,tipo=tipo, fecha__lt=date).aggregate(Sum('valor'))["valor__sum"] == None: 
 		return 0
 	else:
-		return models.Ingreso.objects.filter(cliente=cliente,tipo=tipo).exclude(fecha__month=month,fecha__year=year).aggregate(Sum('valor'))["valor__sum"]
+		return models.Ingreso.objects.filter(cliente=cliente,tipo=tipo, fecha__lt=date).aggregate(Sum('valor'))["valor__sum"]
 
-def deuda_total(cliente, tipo): 
-	if models.Ingreso.objects.filter(cliente=cliente,tipo=tipo).aggregate(Sum('valor'))["valor__sum"] == None: 
+def deuda_total(cliente, tipo, datet): 
+	if models.Ingreso.objects.filter(cliente=cliente,tipo=tipo, fecha__lt=datet).aggregate(Sum('valor'))["valor__sum"] == None: 
 		return 0
 	else:
-		return models.Ingreso.objects.filter(cliente=cliente,tipo=tipo).aggregate(Sum('valor'))["valor__sum"]
+		return models.Ingreso.objects.filter(cliente=cliente,tipo=tipo, fecha__lt=datet).aggregate(Sum('valor'))["valor__sum"]
 
 
 class addCliente(View):
@@ -178,6 +189,8 @@ class filter(TemplateView):
 	def get_context_data(self, **kwargs):
 		context = super(filter, self).get_context_data(**kwargs)
 		context["listGlosa"] = models.Glosa.objects.all().order_by("pk")
+		locale.setlocale(locale.LC_TIME, 'es_ES')
+		datet = datetime.datetime.strptime(self.request.GET["date"], "%B - %Y")
 		if(self.request.GET["filter"]=="all"):
 			context["listCliente"] = models.Cliente.objects.all().order_by("pk")
 		elif self.request.GET["filter"]=="natural":
@@ -186,7 +199,7 @@ class filter(TemplateView):
 			context["listCliente"] = models.Cliente.objects.filter(tipo="sociedad",duenio="").order_by("pk")
 		else:
 			context["listCliente"] = models.Cliente.objects.filter(tipo="sociedad",duenio=self.request.GET["filter"]).order_by("pk")
-		context["clienteGlosa"] = tabla(context["listCliente"],context["listGlosa"])
+		context["clienteGlosa"] = tabla(context["listCliente"],context["listGlosa"], datet)
 		return context
 
 	@method_decorator(login_required)
@@ -202,7 +215,7 @@ class cliente(TemplateView):
 		context["ingreso"] = models.Ingreso.objects.filter(cliente=context["cliente"]).order_by("-fecha")
 		context["deuda"] = context["ingreso"].filter(tipo="deuda").order_by("-fecha")
 		context["boleta"] = context["ingreso"].filter(tipo="boleta").order_by("-fecha")
-		context["balance"] = deuda_total(context["cliente"],"deuda") - deuda_total(context["cliente"],"boleta")
+		context["balance"] = deuda_total(context["cliente"],"deuda",datetime.date.today()) - deuda_total(context["cliente"],"boleta",datetime.date.today())
 		context["formCliente"] = forms.ClienteForm(instance=context["cliente"],prefix="cliente")
 		context["formCobro"] = forms.CobroForm(prefix="id")
 		context["formAbono"] = forms.AbonoForm(prefix="id")	
--- deudas/static-development/js/script.js
+++ deudas/static-development/js/script.js
@@ -2,38 +2,44 @@ $(document).on("ready", function(){
 	$(".datepicker").datepicker({
 		format: "dd/mm/yyyy"
 	});
-	$(".modal-click").on("click",function(){
-		console.log("Hola!");
-		$(".cliente_pk").val($(this).data("id"));
-	});
+	$(".modal-click").on("click", clienteClick);
 	$('.table').dataTable({
 		"language": {
             "url": "//cdn.datatables.net/plug-ins/be7019ee387/i18n/Spanish.json"
         }
 	});
-	$(".edit-click").on("click",function(){
-		$.ajax({
-		  type: "GET",
-		  url: "/edit/cliente/",
-		  data: { pk: $(this).data("id")},
-		  success: function(form){
-		  	console.log("Hola!");
-		  	$("#editCliente .modal-body").html(form);
-		  }
-		})
-	});
-	$("#filter").on("change",function(){
+	$(".edit-click").on("click", editClick);
+	$("#filter, #date").on("change",function(){
 		$.ajax({
 			type: "GET",
 			url: "/filter/",
-			data: {filter: $(this).val()},
+			data: {filter: $("#filter").val(), date: $("#date").val()},
 			success: function(table){
 				$(".data-table").html(table).find(".table").dataTable({
 					"language": {
 			            "url": "//cdn.datatables.net/plug-ins/be7019ee387/i18n/Spanish.json"
 			        }
 				});
+				$(".modal-click").on("click", clienteClick);
+				$(".edit-click").on("click", editClick);
 			}
 		})
 	})
 });
+
+function clienteClick(){
+	console.log("Hola!");
+	$(".cliente_pk").val($(this).data("id"));
+}
+
+function editClick(){
+	$.ajax({
+	  type: "GET",
+	  url: "/edit/cliente/",
+	  data: { pk: $(this).data("id")},
+	  success: function(form){
+	  	console.log("Hola!");
+	  	$("#editCliente .modal-body").html(form);
+	  }
+	})
+}
